133e506a4598009bca41e77575ff48528ceb3d13
Sujin <ccclalasj@gmail.com>
2021-04-22 19:44:03 -0400

    google login

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,112 +1,104 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import GoogleLogin from "react-google-login";
+import { auth } from "../../config/fbConfig";
 import { useDispatch, useSelector } from "react-redux";
 import {
   selectSignedIn,
   setSignedIn,
   setUserData,
 } from "../../features/userSlice";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import "./SignInUp.css";
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+import "./SignInUp.css";
+import { authIsReady } from "react-redux-firebase";
 
 const SignIn = () => {
-  const classes = useStyles();
-  const register = () => {};
-  const loginToApp = () => {};
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
+
+  const loginToApp = (e) => {
+    e.preventDefault();
+  };
+  const resister = () => {
+    if (!name) {
+      return alert("Please enter a full name");
+    }
+    auth.createUserWithEmailAndPassword(email, password).then((userAuth) => {
+      userAuth.user
+        .updateProfile({
+          displayName: name,
+        })
+        .then(() => {
+          dispatch(
+            setSignedIn({
+              email: userAuth.user.email,
+              uid: userAuth.user.uid,
+              displayName: name,
+            })
+          );
+        });
+    });
+  };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
+    <div className="signin">
+      <div class="signin__container">
+        <div className="login__header">
+          <img
+            src="https://cdn.pixabay.com/photo/2015/12/08/19/08/castle-1083570_1280.png"
+            alt=""
           />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
+          <h3>Log In</h3>
+        </div>
+        <form class="col s12 ">
+          <div class="">
+            <div class="input-field col s6">
+              <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                id="first_name"
+                type="text"
+                class="validate"
+                placeholder="Full Name"
+              />
+            </div>
+          </div>
+
+          <div class="">
+            <div class="input-field col s12">
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                id="password"
+                type="password"
+                class="validate"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          <div class="">
+            <div class="input-field col s12">
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                id="email"
+                type="email"
+                class="validate"
+                placeholder="Email"
+              />
+            </div>
+          </div>
         </form>
+        <button className="btn" onClick={loginToApp}>
+          Log In
+        </button>
+        <div className="signin__signup">
+          <span>Not a member?</span>
+          <span onClick={resister}>Resister now</span>
+        </div>
       </div>
-    </Container>
+    </div>
   );
 };
 
