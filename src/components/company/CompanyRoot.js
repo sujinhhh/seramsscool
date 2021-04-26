c4704a3ea5b53382d4b5f36266df7260d7526d22
Sujin <ccclalasj@gmail.com>
2021-04-26 19:54:11 -0400

    sass

--- src/components/company/CompanyRoot.js
+++ src/components/company/CompanyRoot.js
@@ -1,11 +1,18 @@
 import React from "react";
-import "./Company.css";
+import "../../scss/company.scss";
 
 const CompanyRoot = () => {
   return (
     <div>
       <div className="companyRoot">
-        <h1>Comming soon</h1>
+        <h1 className="companyRoot__title">Comming soon</h1>
+        <button className="btn"> click</button>
+      </div>
+      <div className="row">
+        <div className="col-1">hahah</div>
+        <div className="col-2">hahah</div>
+        <div className="col-3">hahah</div>
+        <div className="col-4">hahah</div>
       </div>
     </div>
   );
